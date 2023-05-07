e5a01397b7a4951bd62170c3c65196744780f511
huichihwang <taya87136@gmail.com>
2023-05-07 23:52:33 +0800

    set up some beans related to authentication

---
+++ src/main/java/com/example/loginbackend/configuration/AuthenticationConfiguration.java
@@ -0,0 +1,35 @@
+package com.example.loginbackend.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthenticationConfiguration {
+
+    private final UserDetailsService appUserService;
+    @Bean
+    public AuthenticationProvider authenticationProvider(PasswordEncoder pwdEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(pwdEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
