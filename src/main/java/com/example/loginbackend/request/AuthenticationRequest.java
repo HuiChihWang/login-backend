027e6b94b11dd8b10aa856762fff57f2e80a0469
huichihwang <taya87136@gmail.com>
2023-05-08 17:59:59 +0800

    add validator

--- src/main/java/com/example/loginbackend/request/AuthenticationRequest.java
+++ src/main/java/com/example/loginbackend/request/AuthenticationRequest.java
@@ -1,23 +1,16 @@
 package com.example.loginbackend.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record RegistrationRequest(
+public record AuthenticationRequest(
         @Pattern(regexp = "^[a-zA-Z0-9]+$")
         @Size(min = 7)
         String username,
 
         @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*()-=_+{};':\"\\\\|,.<>/?]).+$")
         @Size(min = 7)
-        String password,
-
-        @Email
-        String email,
-
-        @NotEmpty
-        String name
+        String password
 ) {
 }
+
