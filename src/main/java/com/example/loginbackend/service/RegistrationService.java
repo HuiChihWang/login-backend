f19996e233a6ce0fcf2abe1bc42f066f2e1c46d7
huichihwang <taya87136@gmail.com>
2023-05-08 20:55:13 +0800

    integrate token generation process to registration process

--- src/main/java/com/example/loginbackend/service/RegistrationService.java
+++ src/main/java/com/example/loginbackend/service/RegistrationService.java
@@ -2,6 +2,7 @@ package com.example.loginbackend.service;
 
 import com.example.loginbackend.entity.AppUser;
 import com.example.loginbackend.entity.AppUserRole;
+import com.example.loginbackend.entity.ConfirmationToken;
 import com.example.loginbackend.exception.UserAlreadyExistedException;
 import com.example.loginbackend.repository.AppUserRepository;
 import com.example.loginbackend.request.RegistrationRequest;
@@ -9,6 +10,8 @@ import com.example.loginbackend.request.RegistrationRequest;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +20,15 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class RegistrationService {
+    private static final Logger logger = LoggerFactory.getLogger(RegistrationService.class);
     @NonNull
     private final AppUserRepository userRepository;
+
     @NonNull
     private final PasswordEncoder passwordEncoder;
+
+    @NonNull
+    private final ConfirmationTokenService tokenService;
 
     public AppUser registration(RegistrationRequest request) {
         Optional<AppUser> userWithUsername = userRepository.findAppUserByUsername(request.username());
@@ -42,6 +50,13 @@ public class RegistrationService {
                 AppUserRole.USER
         );
 
-        return userRepository.save(newUser);
+        AppUser savedUser = userRepository.save(newUser);
+
+        // TODO: call token service to generate token
+        ConfirmationToken token = tokenService.generateConfirmationToken(savedUser);
+        logger.info("Register new user {} with confirmation token {}", savedUser.getId(), token.getToken());
+
+        // TODO: sending confirmation Email
+        return savedUser;
     }
 }
